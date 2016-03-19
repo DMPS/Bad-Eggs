124d157558b8f0bfbdc5a786fca74dd60a0dbd6c
Matti Astrand <mattiastr@gmail.com>
2016-03-19 13:27:22 +0000

    Function to find suspicious facts about a company

--- opencorp.py
+++ opencorp.py
@@ -2,24 +2,41 @@ import httplib
 import urllib
 import sys
 import json
+from datetime import datetime,timedelta
 
-conn = httplib.HTTPConnection('api.opencorporates.com')
-params = urllib.urlencode({'q': 'Nokia Oyj'})
-conn.request('GET', '/v0.4.3/companies/search' + '?' + params)
-response = conn.getresponse()
-print response.status, response.reason
+def parse_date(string):
+    if string is None:
+        return None
+    else:
+        return datetime.strptime(string, "%Y-%m-%d")
 
-data = json.loads(response.read())
-print 'Number of results:', data['results']['total_count']
-
-comps = data['results']['companies']
-names = [ c['company']['name'] for c in comps ]
-for name in names:
-    print name
-
-#recent = data['results']['company']['data']['most_recent']
-#for d in recent:
-#    print d['datum']['title']
+today = datetime.today()
 
 def suspicious(company):
-    return True
+    ret_val = {}
+    inc_date = parse_date(company['incorporation_date'])
+    if inc_date is None or today - inc_date < timedelta(1000):
+        ret_val['inc_date'] = inc_date
+    if company['dissolution_date'] is not None:
+        ret_val['dissolution_date'] = company['dissolution_date']
+    return ret_val
+
+def fetch_companies(search_term):
+    conn = httplib.HTTPConnection('api.opencorporates.com')
+    params = urllib.urlencode({'q': search_term})
+    conn.request('GET', '/v0.4.3/companies/search' + '?' + params)
+    response = conn.getresponse()
+    #print response.status, response.reason
+
+    data = json.loads(response.read())
+    print 'Number of results:', data['results']['total_count']
+    return [ c['company'] for c in data['results']['companies'] ]
+
+name = 'Nokia Oyj'
+if len(sys.argv) > 1:
+    name = sys.argv[1]
+
+comps = fetch_companies(name)
+for co in comps:
+    print co['name'], suspicious(co)
+
