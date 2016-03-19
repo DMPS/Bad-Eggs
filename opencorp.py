afb388865e660ee9dfa4cfb423bec5a831ba25e6
Matti Astrand <mattiastr@gmail.com>
2016-03-19 11:08:50 +0000

    search by company name

--- opencorp.py
+++ opencorp.py
@@ -1,13 +1,19 @@
 import httplib
+import urllib
 import sys
 import json
 
 conn = httplib.HTTPConnection('api.opencorporates.com')
-conn.request('GET', '/companies/nl/17087985')
+params = urllib.urlencode({'q': 'Tesco'})
+conn.request('GET', '/v0.4.3/companies/search' + '?' + params)
 response = conn.getresponse()
 print response.status, response.reason
 
 data = json.loads(response.read())
-recent = data['results']['company']['data']['most_recent']
-for d in recent:
-    print d['datum']['title']
+comps = data['results']['companies']
+names = [ c['company']['name'] for c in comps ]
+for name in names:
+    print name
+#recent = data['results']['company']['data']['most_recent']
+#for d in recent:
+#    print d['datum']['title']
